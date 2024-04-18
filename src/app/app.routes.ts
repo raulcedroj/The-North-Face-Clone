1bfcfc4832ba4772aecabbe60e4ca978e2c7dd85
raulcedroj <raulcedroj@campus.monlau.com>
2024-04-18 15:35:34 +0200

    primer commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ProductsComponent } from './components/products/products.component';
+import { AdminComponent } from './components/admin/admin.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'home', component: HomeComponent
+    },
+    {
+        path: 'products', component: ProductsComponent
+    },
+    {
+        path: 'admin', component: AdminComponent
+    },
+    {
+        path: '', redirectTo: '/home', pathMatch: 'full'
+    }
+];
